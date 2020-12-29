f528b81e7c1b80bdd63bd75102ed514419c16844
aniledev <elinamcgill@gmail.com>
2020-12-29 11:55:21 -0500

    feat: validate the genre parameter

--- app.js
+++ app.js
@@ -25,6 +25,22 @@ app.get("/apps", (req, res) => {
       return res.status(400).send("Sort must be one of rating or app");
     }
   }
+
+  // if there is a genre, it must also be validated
+  if (genres) {
+    if (
+      !["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"].includes(
+        genres
+      )
+    ) {
+      // if genres is not one of the 6, then return a status of 400
+      return res
+        .status(400)
+        .send(
+          "Genre must either be Action, Puzzle, Strategy, Casual, Arcade or Card"
+        );
+    }
+  }
 });
 
 // listen for the app on the port 8000
